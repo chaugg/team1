91bc831711e22f7c879e01f24976daae99ecedca
Nguyen Tran Chau Giang <tothelegendary@gmail.com>
2017-04-15 13:28:46 +0700

    Sign up page

--- signup.php
+++ signup.php
@@ -1,3 +1,31 @@
+<?php
+include_once 'resource/Database.php';
+// dump
+//var_dump($_POST);
+if (isset($_POST['email'])){
+  $email = $_POST['email'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  try {
+    $sqlInsert = "INSERT INTO users (username, email, password, join_date)
+    VALUES (:username, :email, :password, now())";
+
+    $statement = $db->prepare($sqlInsert);
+    $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $password));
+
+    if($statement->rowCount() == 1){
+      $result = "<p style = 'padding: 20px; color:green;'> Registration
+      successful</p>";
+    }
+  } catch (PDOException $ex) {
+    $result = "<p style = 'padding: 20px; color:red;'> Error! ".$ex->getMessage()."
+    </p>";
+  }
+}
+ ?>
+
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -6,18 +34,18 @@
 </head>
 <body>
 <h2>User Authentication System </h2><hr>
-
 <h3>Register Form</h3>
+<?php if (isset($result)) echo $result; ?>
 <form method = "post" action="">
   <table>
     <tr>
-      <td>Email:</td><td><input type="text" name="" value=""></td>
+      <td>Email:</td><td><input type="text" name="email" value=""></td>
     </tr>
     <tr>
-      <td>Username:</td><td><input type="text" name="" value=""></td>
+      <td>Username:</td><td><input type="text" name="username" value=""></td>
     </tr>
     <tr>
-      <td>Password:</td><td><input type="text" name="" value=""></td>
+      <td>Password:</td><td><input type="text" name="password" value=""></td>
     </tr>
     <tr>
       <td></td><td><input style ="float : right" type="submit" name="" value="Signup"></td>
