fe53349e996fa2a29a6fc70039c3f3abef50d9f0
Nguyen Tran Chau Giang <tothelegendary@gmail.com>
2017-04-15 17:12:04 +0700

    Finish login system

--- signup.php
+++ signup.php
@@ -2,30 +2,23 @@
 include_once 'resource/Database.php';
 include_once 'resource/utilities.php';
 
-
 if(isset($_POST['signupBtn'])){
-
     $form_errors = array();
-
-    //Form validation
     $required_fields = array('email', 'username', 'password');
+
     $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
     $fields_to_check_length = array('username' => 4, 'password' => 6);
     $form_errors = array_merge($form_errors, check_min_length($fields_to_check_length));
     $form_errors = array_merge($form_errors, check_email($_POST));
-
     if(empty($form_errors)){
         $email = $_POST['email'];
         $username = $_POST['username'];
         $password = $_POST['password'];
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
         try{
-
-            //create SQL insert
             $sqlInsert = "INSERT INTO users (username, email, password, join_date)
               VALUES (:username, :email, :password, now())";
             $statement = $db->prepare($sqlInsert);
-            //add the data into the database
             $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
             if($statement->rowCount() == 1){
                 $result = "<p style='padding:20px; border: 1px solid gray; color: green;'> Registration Successful</p>";
