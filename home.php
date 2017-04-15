071b034a88b51eaa8e70b7966cbdb76aefeff7f0
nguyen tran chau giang <tothelegendary@gmail.com>
2017-04-16 06:11:29 +0700

    Add profile

--- home.php
+++ home.php
@@ -1,41 +1,6 @@
 <?php
 include_once 'resource/session.php';
-include_once 'resource/Database.php';
-include_once 'resource/utilities.php';
 
-if(isset($_POST['loginBtn'])){
-    $form_errors = array();
-    $required_fields = array('username', 'password');
-    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
-    if(empty($form_errors)){
-        $user = $_POST['username'];
-        $password = $_POST['password'];
-        $sqlQuery = "SELECT * FROM users WHERE username = :username";
-        $statement = $db->prepare($sqlQuery);
-        $statement->execute(array(':username' => $user));
-
-       while($row = $statement->fetch()){
-           $id = $row['id'];
-           $hashed_password = $row['password'];
-           $username = $row['username'];
-
-           if(password_verify($password, $hashed_password)){
-               $_SESSION['id'] = $id;
-               $_SESSION['username'] = $username;
-               header("location: home.php");
-           }else{
-               $result = "<p style='padding: 20px; color: red; border: 1px solid gray;'> Invalid username or password</p>";
-           }
-       }
-
-    }else{
-        if(count($form_errors) == 1){
-            $result = "<p style='color: red;'>There was one error in the form </p>";
-        }else{
-            $result = "<p style='color: red;'>There were " .count($form_errors). " error in the form </p>";
-        }
-    }
-}
 ?>
 <!DOCTYPE html>
 <html >
@@ -73,31 +38,12 @@ if(isset($_POST['loginBtn'])){
   <?php else: ?>
     <p class="userbar">Welcome back <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a> </p>
   <?php endif ?>
+
 </div>
 
 <body>
   <header class="site-header" role="banner">
 
-    <div class="topbar">
-        <h1 class="site-title"><a href="#">
-
-        <!-- Logo  -->
-
-
-        <!-- Logo  -->
-
-        </a></h1>
-
-        <nav class="top-navigation" role="navigation">
-            <ul class="inline-list padded-list">
-                    <li><a href="#" class="ion-social-octocat"> Wiki</a></li>
-
-                    <li><a href="../form/register.php" class="ion-ios-paw"> Register</a></li>
-
-                    <li><a href="../form/login.php" class="icon ion-android-exit button button-red button-login toggle-hide ">Sign In</a></li>
-            </ul>
-        </nav>
-    </div>
 
     <div class="header-image">
         <div class="header-image-text">
@@ -112,118 +58,40 @@ if(isset($_POST['loginBtn'])){
 <main role="main" class="site-main">
 
     <section class="fixed-width highlighted-bg">
-        <h2>How to use this website</h2>
+        <h2>Pick a lesson</h2>
 
         <div class="col fourth">
-            <h3>Get Connected</h3>
+            <h3>Marugoto</h3>
             <img src="img/1.svg" alt="" class="landing-icon">
-            <p>Join a class.</p>
+
         </div>
 
         <div class="col fourth">
-            <h3>Pick a lesson</h3>
+            <h3>Minna</h3>
             <img src="img/2.svg" alt="" class="landing-icon">
-            <p>Work together.</p>
+
         </div>
 
         <div class="col fourth">
-            <h3>Learn everywhere</h3>
+            <h3>Live class</h3>
             <img src="img/3.svg" alt="" class="landing-icon">
-            <p>Save your time.</p>
+
         </div>
 
         <div class="col fourth">
-            <h3>Nonprofits</h3>
+            <h3>For beginer</h3>
             <img src="img/4.svg" alt="" class="landing-icon">
-            <p>Always free</p>
+
         </div>
     </section>
 
     <section class="fixed-width">
-        <h2>Features</h2>
 
-        <div class="col fourth highlighted-bg small-box">
-            <h3>One</h3>
-            <div class="icon icon-large ion-android-wifi"></div>
-        </div>
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Two</h3>
-            <div class="icon icon-large ion-android-favorite"></div>
-        </div>
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Three</h3>
-            <div class="icon icon-large ion-android-calendar"></div>
-        </div>
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Four</h3>
-            <div class="icon icon-large ion-android-star"></div>
-        </div>
-
-
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Five</h3>
-            <div class="icon icon-large ion-android-boat"></div>
-        </div>
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Six</h3>
-            <div class="icon icon-large ion-android-happy"></div>
-        </div>
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Seven</h3>
-            <div class="icon icon-large ion-social-snapchat"></div>
-        </div>
-
-        <div class="col fourth highlighted-bg small-box">
-            <h3>Eight</h3>
-            <div class="icon icon-large ion-social-freebsd-devil"></div>
-        </div>
-
-    </section>
-
-    <section class="fixed-width highlighted-bg">
-        <h2>Our leader</h2>
-
-        <div class="col third">
-            <img src="img/1.jpg" alt="" class="landing-icon testimonial-avatar">
-            <p>@admin</a></p>
-        </div>
-
-        <div class="col third">
-            <img src="img/2.jpg" alt="" class="landing-icon testimonial-avatar">
-            <p>Boost up your skill!
-            - <a href="#">@admin</a></p>
-        </div>
-
-        <div class="col third">
-            <img src="img/3.jpg" alt="" class="landing-icon testimonial-avatar">
-            <p>Welcoming!
-                - <a href="https://twitter.com/cynthialanel">@admin</a>
-            </p>
-        </div>
 
     </section>
 
     <section class="fixed-width">
-        <h2>Why you should join :</h2>
 
-        <ul class="large-list">
-            <li class="ion-lock-combination">Get help real time.</li>
-            <li class="ion-lock-combination">Lorem up sum.</li>
-            <li class="ion-lock-combination">YLorem up sum.</li>
-            <li class="ion-lock-combination">Lorem up sum .</li>
-            <li class="ion-lock-combination">Lorem up sum <strong>Nihongo</strong> Lorem up sum.</li>
-            <li class="ion-lock-combination">Lorem up sum'll Lorem up sum.</li>
-            <li class="ion-lock-combination">Lorem up sum.</li>
-            <li class="ion-lock-combination">(it's free)</li>
-        </ul>
-
-        <a href="" class="button button-yellow">Started</a>
     </section>
 
     <section class="fixed-width">
@@ -250,7 +118,7 @@ if(isset($_POST['loginBtn'])){
 <div id="modal-bg" class="login-modal-bg toggle-hide"></div>
 <div id="login" class="login-container">
     <div class="col half">
-        <h3>Sign in with username</h3>
+        <h3>Sign in with email</h3>
         <?php if(isset($result)) echo $result; ?>
         <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
         <form method="post" action="">
